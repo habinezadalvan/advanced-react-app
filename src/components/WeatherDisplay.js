5fb8fb805c4410487d748c6613eaae66bb3817df
habinezadalvan <habinezadalvan@gmail.com>
2020-07-07 19:44:07 +0200

    cold weather

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -10,6 +10,8 @@ const warmNight =
   "https://images.pexels.com/photos/1154610/pexels-photo-1154610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
 const sunnyDay =
   "https://images.pexels.com/photos/3768/sky-sunny-clouds-cloudy.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+const coldNight = "https://images.pexels.com/photos/358235/pexels-photo-358235.jpeg?cs=srgb&dl=atmosfeer-bevroren-bomen-dageraad-358235.jpg&fm=jpg";
+const coldMorning ="https://images.pexels.com/photos/2975540/pexels-photo-2975540.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
 
 const WeatherDisplay = ({ temp, time }) => {
 
@@ -18,10 +20,26 @@ const WeatherDisplay = ({ temp, time }) => {
     <div className="container">
       {temp <= 18 ? (
         <div className="card">
-          <p>It's cold outside</p>
-          <span>The current temperature is {temp} Degree Celcius</span>
+        {hour >= 6 && hour <= 12 ? (
+          <img src={coldMorning} alt="nature" className="img" />
+        ) : hour > 12 && hour < 18 ? (
+          <img src={coldMorning} alt="warm afternoon" className="img" />
+        ) : (
+          <img src={coldNight} alt="night" className="img" />
+        )}
+        <div className="img model"></div>
+        <div className="text">
+          <p>
+            It's <span className="warm">Cold</span> outside
+          </p>
+          <p>The current temperature is</p>
+          <span className="warm">
+            {" "}
+            {temp} <span>&#8451;</span>
+          </span>
         <p>{time}</p>
         </div>
+      </div>
       ) : temp > 18 && temp <= 25 ? (
         <div className="card">
           {hour >= 6 && hour <= 12 ? (
