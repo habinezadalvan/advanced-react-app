3ae97e3281a53c75cc2dd69a3dfb1def20039231
habinezadalvan <habinezadalvan@gmail.com>
2020-10-16 08:39:20 +0200

    change profile picture

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,8 @@
+module.exports = {
+    findProcess: 'FIND_PROCESS',
+    sendMessage: 'SEND_MESSAGE',
+    sendMessageError: 'SEND_MESSAGE_ERROR',
+    members: 'FETCH_MEMBERS',
+    department: 'IT_DEPARTMENT',
+    BACK_END_BASE_URL: 'https://digitol-api.herokuapp.com',
+}
--- src/assets/imageUrls.js
+++ src/assets/imageUrls.js
@@ -1,5 +1,5 @@
 module.exports = {
-    profile: 'https://res.cloudinary.com/habinezadalvan/image/upload/v1599053017/isake_njclgn.png',
+    profile: 'https://res.cloudinary.com/habinezadalvan/image/upload/v1602828224/digitol/american_png-min_g3zzrp.png',
     authorsHaven: 'https://res.cloudinary.com/habinezadalvan/image/upload/v1572422285/nep0vrer3xnglkvfpxp4.png',
     ligLogo: 'https://res.cloudinary.com/habinezadalvan/image/upload/v1599115915/digitol/small_logo_d4phgy.png',
     digitologo: 'https://res.cloudinary.com/habinezadalvan/image/upload/v1599136876/digitol/digitologo_jk1sal.png',
